a8aa089d89f65e05e98b1c1c828750c225c831bc
Marc Szymanski <marc.szymanski@adesso.de>
2022-12-16 15:33:22 +0100

    construct returns a dict with all created shapes

--- Airplane/ConstructionStepNode.py
+++ Airplane/ConstructionStepNode.py
@@ -11,7 +11,7 @@ class ConstructionStepNode(AbstractConstructionStep, MutableMapping):
     A node that is a map and holds in itself the following steps in the construction tree
     """
 
-    def __init__(self, creator: AbstractShapeCreator, successors=None):
+    def __init__(self, creator: AbstractShapeCreator, successors=None, **kwargs):
         """
         :param geometry: the geometry, that is created in this node
         :param successors: all following construction steps
@@ -42,17 +42,17 @@ class ConstructionStepNode(AbstractConstructionStep, MutableMapping):
         """
         self.update({value.creator.identifier: value})
 
-    def construct(self, input_shapes: dict[str, tgl_geom.CNamedShape] = None, **kwargs):
+    def construct(self, input_shapes: dict[str, tgl_geom.CNamedShape] = None, **kwargs) -> dict[str, object]:
         """
         Executes the construction of all shapes based on the defined workflow structure.
         :param input_shapes: the shapes that have been constructed in the last step
-        :param kwargs: holding the shapes of the previous steps as a dict of shape lists (input_shapes is the last entry of this dict)
-        :return: a structure based on the identifiers, that represents the workflow
+        :param kwargs: holding the shapes of the previous steps as a dict of shape lists
+        (input_shapes is the last entry of this dict)
+        :return: a dict with all shapes created during the workflow
         """
         self._output_shapes = self.creator.create_shape(input_shapes=input_shapes, **kwargs)
         kwargs.update(self._output_shapes)
-        output_list: list[tgl_geom.CNamedShape] = []
+        output_dict = self._output_shapes
         for key in self.successors:
-            output_list.append(self.successors.get(key).construct(input_shapes=self._output_shapes, **kwargs))
-        output_dict = {self.creator.identifier: {"shapes": self._output_shapes, "steps": output_list}}
+            output_dict.update(self.successors.get(key).construct(input_shapes=self._output_shapes, **kwargs))
         return output_dict
