c972704a668ed567f81bfd33af0e6eacad813d24
szymanski <marc.szymanski@mac.com>
2025-05-01 16:14:06 +0200

    disable display

--- cad_designer/cq_plugins/display/display.py
+++ cad_designer/cq_plugins/display/display.py
@@ -2,13 +2,13 @@ import logging
 import requests
 
 from cadquery import Workplane
-from cad_designer.cq_plugins.display.cadq_server_connector import CQServerConnector
 from cad_designer.decorators.general_decorators import conditional_execute
 
 
 @conditional_execute("DISPLAY_CONSTRUCTION_STEP")
 def display(self: Workplane, name: str = "NN", severity: int = logging.DEBUG,
              url: str = "http://cq-server:5000/json", names=None, colors=None, alphas=None, **kwargs) -> Workplane:
+    from cad_designer.cq_plugins.display.cadq_server_connector import CQServerConnector
     if severity >= logging.root.level:
         display._connector = display._connector if display._connector is not None else CQServerConnector(url=url)
         try:
